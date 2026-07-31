4fbb0f2153b04efd1b1fd7ce58883b55007c4f3e
takahashiumaru <umarmarufmutaqin@gmail.com>
2026-07-31 11:03:36 +0700

    feat(api): support optional ratings include in products index

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -4,12 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return static::paginatedResponse(Product::paginate(10));
+        $products = Product::paginate(10);
+
+        if ($request->query('include') === 'ratings') {
+            $products->getCollection()->transform(function ($product) {
+                $stats = self::getProductRatingStats($product->id);
+                $product->rating_avg = $stats['avg'];
+                $product->rating_count = $stats['count'];
+
+                return $product;
+            });
+        }
+
+        return static::paginatedResponse($products);
     }
 
     public function show($id)
